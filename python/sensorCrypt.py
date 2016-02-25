f97f84b20a5f824cd46dd74af251830dbd8f7b30
zodiac612 <sb240@gmx.de>
2016-02-25 09:08:48 +0100

    Service start with sleep 30 and ntp restart; changed key for crypt

--- python/sensorCrypt.py
+++ python/sensorCrypt.py
@@ -4,11 +4,11 @@ import datetime
 
 class sensorCrypt(object):
         def __init__(self, vCryptKey):
-            self.__key = hashlib.sha512(str(vCryptKey+'salt')).hexdigest()
-            self.__lenKey = len(self.__key)
+            self.__vSalt = 'Qr1#Y'
+            self.__lenKey = len(vCryptKey+self.__vSalt)
             self.__crypt_position = self.__GetCryptPosition()
+            self.__key = hashlib.sha512(str(vCryptKey+self.__vSalt+str(self.__crypt_position))).hexdigest()
             print self.__crypt_position
-            
 
         def Encrypt(self, vString):
             # print'######'
@@ -21,7 +21,7 @@ class sensorCrypt(object):
                   
         def Decrypt(self, vString):
             ilenMesg = len(vString)
-            vBase64 = vString[0:self.__crypt_position] + vString[self.__crypt_position+self.__lenKey:len(vString)]
+            vBase64 = vString[0:self.__crypt_position] + vString[self.__crypt_position:len(vString)]
             # print vBase64
             messageData = base64.b64decode(vBase64)
             # print messageData
@@ -38,15 +38,18 @@ class sensorCrypt(object):
             qs1 = self.__querSum(vDate1)
             qs2 = self.__querSum(vDate2)
             qs3 = self.__querSum(self.__lenKey)
-            # print str(qs0) + '#' + str(qs1) + '#' + str(qs2)
+            # print str(qs0) + '#' + str(qs1) + '#' + str(qs2) + '#' + str(qs3)
             variant1 = int(round(qs0/9))
             variant2 = int(round(int(vDate1)/qs1))
             variant3 = int(round(variant2/(variant1+qs1)))
             # print str(variant1) + '#' + str(variant2) + '#' + str(variant3)
             vResult = qs0 + variant1 + variant3 + qs3
+            # print vResult
             if vResult > 100:
                 vResult = str(vResult)[-2:]
-            return vResult
+                # print vResult
+            vResult = int(vResult)+7
+            return int(vResult)
         
         def __querSum(self, vZahl):
             zahlString=str(vZahl)

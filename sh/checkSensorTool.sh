9980183096336f44ca12bca87e8645fcdb0addc3
zodiac612 <sb240@gmx.de>
2016-09-16 11:28:26 +0200

    removed whatsapp message for service status

--- sh/checkSensorTool.sh
+++ sh/checkSensorTool.sh
@@ -4,13 +4,13 @@ PIDLA=`ps -ef | grep sensorService.py | awk '$0!~/grep/ && $2~/[0-9]/{print $2}'
 PIDIN=`ps -ef | grep sensorLogic.py | awk '$0!~/grep/ && $2~/[0-9]/{print $2}'`;
 
 if [ -z "$PIDIN" ] ; then
-    /home/pi/yowsup/yowsup-cli demos -s 4917634546625 "in3monitor.py stop unexpectly!" -c /home/pi/yowsup/config
+    #/home/pi/yowsup/yowsup-cli demos -s 4917634546625 "in3monitor.py stop unexpectly!" -c /home/pi/yowsup/config
     echo "sensorLogic.py stop unexpectly!"
 else
     echo "sensorLogic.py PID" $PIDIN
 fi
 if [ -z "$PIDLA" ] ; then
-    /home/pi/yowsup/yowsup-cli demos -s 4917634546625 "Lacrosse.py stop unexpectly!" -c /home/pi/yowsup/config
+    #/home/pi/yowsup/yowsup-cli demos -s 4917634546625 "Lacrosse.py stop unexpectly!" -c /home/pi/yowsup/config
     echo "sensorHttpService.py stop unexpectly!" 
 else
     echo "sensorHttpService.py PID" $PIDLA
